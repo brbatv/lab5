3bad799f834a4e93d8fbfdecf782eaa6b2be993d
brbatv <barakatbruno@gmail.com>
2017-09-30 13:49:54 +0200

    convert.delimiter used outside other function, added key at the end of url

--- R/utilities.R
+++ R/utilities.R
@@ -2,13 +2,11 @@
 
 add_api_key_to_url<-function(url) #simply adds my api key to a google api request at the end
 {
-  return(paste0(url,"AIzaSyApGO2vCAE8dw7MhpKN2esy7cIDxtk8PiY"))
+  return(paste0(url,"&key=AIzaSyApGO2vCAE8dw7MhpKN2esy7cIDxtk8PiY"))
 }
 
 get_json_response_from_url<-function(url){ #takes raw response from server. we may need to clean it
   require(httr)
-  #clean data ? remove first elements of the list maybe
-  
 
   return(content(GET(url)))
 
@@ -16,36 +14,34 @@ get_json_response_from_url<-function(url){ #takes raw response from server. we m
 
 }
 
-url<-add_api_key_to_url("https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=")
+url<-add_api_key_to_url("https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA")
 
+#takes a string, splits it where it find old char and replace it with new
+convert.delimiter <- function (string, old=" ", new = "+")
+{
+  return(paste (unlist (strsplit (string, old)), collapse=new))
+}
 
 #geocoding
 create_url_from_address<-function(address,...)
 {
-  
-  #take an argument and convert it into + format
-  convert.delimiter <- function (string, old=" ", new = "+")
-  {
-    paste (unlist (strsplit (string, old)), collapse=new)
-  }
-  
-  address_n<-convert.delimiter(address)
+   address_n<-convert.delimiter(address)
   url <-paste0("https://maps.googleapis.com/maps/api/geocode/json?address=", address_n)
   
   if(missing(...)) {
     
-    return(url)
+    return(add_api_key_to_url(url))
     
   } else {
     
     options<-lapply(list(...),convert.delimiter)
     options_wname<-paste(names(options),options,sep="=",collapse=",")
-    options_past<- paste(unlist (strsplit (options_wname, ",")), collapse="&")
-    
+    options_past<-paste(unlist (strsplit (options_wname, ",")), collapse="&")
+    options_past<- convert.delimiter(options_wname,old=",",new="&") 
     details <-paste(address_n, options_past ,sep="&",collapse=",")
     url_n <-paste0("https://maps.googleapis.com/maps/api/geocode/json?address=",details)
     
-    return(url_n)
+    return(add_api_key_to_url(url_n))
     
   }
 }
@@ -54,10 +50,7 @@ create_url_from_address<-function(address,...)
 #reverse geocoding
 create_url_from_coordinates<-function(long,lat,...)
 {
-  convert.delimiter <- function (string, old=" ", new = "+")
-  {
-    paste (unlist (strsplit (string, old)), collapse=new)
-  }
+ 
   
   latlng<-paste(lat,long,sep=",")
   
@@ -65,7 +58,7 @@ create_url_from_coordinates<-function(long,lat,...)
   
   if(missing(...)) {
     
-    return(url)
+    return(add_api_key_to_url(url))
     
   } else {
     
@@ -76,7 +69,7 @@ create_url_from_coordinates<-function(long,lat,...)
     details <-paste(latlng, options_past ,sep="&",collapse=",")
     url_n <-paste0("https://maps.googleapis.com/maps/api/geocode/json?latlng=",details)
     
-    return(url_n)
+    return(add_api_key_to_url(url_n))
     
   }
 }
