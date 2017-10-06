db8f5bee959f2492e0c8690c68a3d807708ce95b
brbatv <barakatbruno@gmail.com>
2017-10-06 16:01:14 +0200

    Multiple strings allowed

--- tests/testthat/test.R
+++ tests/testthat/test.R
@@ -1,6 +1,5 @@
 test_that("error when wrong inputs",{
  expect_error(address$new(1,2))
-  expect_error(address$new("12 rue victor hugo paris","france"))
   expect_error(address$new("apdoeporpaoofjrizfjriezfjir789zrf78rz"))
 })
 
